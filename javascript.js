bbe475828cad151758b1856be2bb09727d8558a4
Wally132 <wallacechen132@gmail.com>
2024-05-11 12:55:12 -0700

    adding ui

--- javascript.js
+++ javascript.js
@@ -18,26 +18,16 @@ function getComputerChoice() {
     } 
     }
 
-function getPlayerChoice() {
-    let playerInput = prompt("rock, paper or scissors?");
-    if (playerInput.toLowerCase() === "rock") {
-        return "rock";
-    } else if (playerInput.toLowerCase() === "paper") {
-        return "paper";
-    } else if (playerInput.toLowerCase() === "scissors") {
-        return "scissors";
-    } else { playerInput.toLowerCase() 
-        return "invalid";
-    }
-    }
+
 
 /* Declare a function to plays a single round of the game */
 /* Determine which variables beat which ones */
 /* Determine who is the winner. Either the computer or the player */
 /* Increment 1 point for the winner */
-let playerPoints = 0;
-let computerPoints = 0;
+
 function playRound(playerSelection, computerSelection) {
+    let playerPoints = 0;
+    let computerPoints = 0;
     if (playerSelection == computerSelection) {
         return "It's a Tie!";
     } if (playerSelection == "invalid") {
@@ -52,11 +42,45 @@ function playRound(playerSelection, computerSelection) {
         return `You lose! ${computerSelection} beats ${playerSelection}`;
 }
 
+const rockButton = document.querySelector('#rock-btn');
+const displayResult = document.querySelector('#container');
+rockButton.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    const result = playRound('rock', computerChoice);
+    console.log(result);
+    keepScore();
+    
+});
+
+const paperButton = document.querySelector('#paper-btn');
+paperButton.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    const result = playRound('paper', computerChoice);
+    console.log(result);
+    keepScore();
+});
+
+const scissorsButton = document.querySelector('#scissors-btn');
+scissorsButton.addEventListener("click", () => {
+    const computerChoice = getComputerChoice();
+    const result = playRound('scissors', computerChoice);
+    console.log(result);
+    keepScore();
+});
+
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
 /* Function to play game */
 /* 5 rounds (log function 5 times) */
 /* Display score up to 5 */
 /* Higher score wins game */
-function playGame() {
+/*function playGame() {
     console.log(playRound(getPlayerChoice(), getComputerChoice()));
     console.log(`Player Points: ${playerPoints} Computer Points: ${computerPoints}`);
     console.log(playRound(getPlayerChoice(), getComputerChoice()));
@@ -75,3 +99,4 @@ function playGame() {
 }
 
 playGame();
+*/
