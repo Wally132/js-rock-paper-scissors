c901e988d00d65ba0f76a47dfab5f8cf8d207360
Wally132 <wallacechen132@gmail.com>
2024-05-11 18:26:35 -0700

    finished ui

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
-/* 
-1. Make a fuction for computer choice to generate random output of rock, paper, or scissors.
-2. Make user input input
-3. Compare the input and run the fuction with if...else
-4. Return declaration with string
-*/
+/* Assign starting points to global scope */
+/* Select score container div in global scope */
 
-/*
-1. Declare a fuction for computer choice to generate random output of rock, paper, or scissors */
+let playerScore = 0;
+let computerScore = 0;
+const scoreDisplay = document.querySelector('#score-container');
+
+/* Funtion to randomly generate a number between 1-3 and returns rock, paper or scissors
+for computer input */
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) +1;
     if (randomNumber === 1) {
@@ -16,38 +17,50 @@ function getComputerChoice() {
     } else {
     return "scissors";
     } 
-    }
+};
 
-
-
-/* Declare a function to plays a single round of the game */
-/* Determine which variables beat which ones */
-/* Determine who is the winner. Either the computer or the player */
-/* Increment 1 point for the winner */
+/* Function to play a round of the game by comparing parameters playerSeletion
+and computerSelection. Points are incremented depending on who wins the round.*/
 
 function playRound(playerSelection, computerSelection) {
-    let playerPoints = 0;
-    let computerPoints = 0;
     if (playerSelection == computerSelection) {
         return "It's a Tie!";
-    } if (playerSelection == "invalid") {
-        return "Invalid response, permabanned";
     } else if (playerSelection == "rock" && computerSelection == "scissors" || 
     playerSelection == "paper" && computerSelection == "rock" ||
     playerSelection == "scissors" && computerSelection == "paper") {
-        playerPoints++;
+        playerScore++;
         return `You win! ${playerSelection} beats ${computerSelection}`;
-    } else 
-        computerPoints++;
+    } else {
+        computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
-}
+    }
+};
+
+/* Function to keep and display score. This replaced the original function to
+play until 5 wins. */
+
+function keepScore() {
+    scoreDisplay.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    if (playerScore === 5) {
+        alert ("FUCK YOU BUT YOU WIN");
+    } else if (computerScore === 5) {
+        alert ("FUCK YOU LOSER, GET FUCKED HAHAHAHA");
+    }
+};
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    scoreDisplay.textContent = 'Player: 0, Computer: 0';
+};
+
+/* 3 separate buttons to select player choice */
 
 const rockButton = document.querySelector('#rock-btn');
-const displayResult = document.querySelector('#container');
 rockButton.addEventListener("click", () => {
     const computerChoice = getComputerChoice();
     const result = playRound('rock', computerChoice);
-    console.log(result);
+    document.querySelector('#game-result').textContent = result;
     keepScore();
     
 });
@@ -56,7 +69,7 @@ const paperButton = document.querySelector('#paper-btn');
 paperButton.addEventListener("click", () => {
     const computerChoice = getComputerChoice();
     const result = playRound('paper', computerChoice);
-    console.log(result);
+    document.querySelector('#game-result').textContent = result;
     keepScore();
 });
 
@@ -64,39 +77,11 @@ const scissorsButton = document.querySelector('#scissors-btn');
 scissorsButton.addEventListener("click", () => {
     const computerChoice = getComputerChoice();
     const result = playRound('scissors', computerChoice);
-    console.log(result);
+    document.querySelector('#game-result').textContent = result;
     keepScore();
 });
 
-const container = document.querySelector("#container");
+/* Create a button to reset the game */
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
-
-container.appendChild(content);
-
-/* Function to play game */
-/* 5 rounds (log function 5 times) */
-/* Display score up to 5 */
-/* Higher score wins game */
-/*function playGame() {
-    console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    console.log(`Player Points: ${playerPoints} Computer Points: ${computerPoints}`);
-    console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    console.log(`Player Points: ${playerPoints} Computer Points: ${computerPoints}`);
-    console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    console.log(`Player Points: ${playerPoints} Computer Points: ${computerPoints}`);
-    console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    console.log(`Player Points: ${playerPoints} Computer Points: ${computerPoints}`);
-    console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    console.log(`Player Points: ${playerPoints} Computer Points: ${computerPoints}`);
-    if (playerPoints > computerPoints) {
-        console.log("You Win The Game!");
-    } else {
-        console.log("The House Always Wins");
-    }
-}
-
-playGame();
-*/
+const resetButton = document.querySelector('#reset-btn');
+resetButton.addEventListener("click", resetGame);
